4a6481112040b8d7af2761711ef8a69ba3f170bf
ybekar02 <y_bekar@hotmail.com>
2019-08-11 15:24:30 -0500

    added

--- src/Calculator.java
+++ src/Calculator.java
@@ -14,7 +14,9 @@ public class Calculator {
         return num1 * num2;
     }
 
-    
+    public static double divide(double num1, double num2) {
+        return num1 / num2;
+    }
     //finalized
     //bye
 
