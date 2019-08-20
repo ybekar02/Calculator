8ae2af69a23523343bfec6276c7327724b783b92
ybekar02 <y_bekar@hotmail.com>
2019-08-20 16:13:50 -0500

    f

--- src/Welcome.java
+++ src/Welcome.java
@@ -2,5 +2,6 @@ public class Welcome {
     public static void main(String[] args) {
         System.out.println("really welcomed!");
         System.out.println("and have a good day");
+        System.out.println("today is what day?");
     }
 }
