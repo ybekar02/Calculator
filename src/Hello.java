bd65ca2d1dc5fd8255a0f7c0d43de42137cd504a
ybekardev <y_bekar@hotmail.com>
2021-08-16 15:53:12 -0500

    test

--- src/Hello.java
+++ src/Hello.java
@@ -1,8 +1,9 @@
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
-        System.out.println("Learning java and automation skills");
+        System.out.println("Hello world!");
+        System.out.println("Learning java and automation skills!");
+        System.out.println("Master branch edits...");
     }
 
 }
